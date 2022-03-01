02a20320dffb01e5449677a607ce9e9e7eceba49
enkhamar1220@gmail.com <enkhamar1220@gmail.com>
2022-03-01 10:05:34 +0800

    change

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,7 @@ import Footer from './Footer';
 
 const Layout = ({ children }) => {
 	return (
-		<Box sx={{ minHeight: '100vh' }} classaName="layout">
+		<Box sx={{ minHeight: '100vh' }} className="layout">
 			{children}
 			<Footer title="RE/MAX PEAK" description="БАЯНЗҮРХ ДҮҮРГИЙН 26-Р ХОРООНЫ ОРШИН СУУГЧИДДАА ЗОРИУЛАН БҮТЭЭВ" url='https://www.facebook.com/remaxpeakinmongolia' />
 		</Box>
